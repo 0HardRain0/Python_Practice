682267c9e004f389e33c5b21a30246f9658569b1
Wooyeol Baek <45348509+0HardRain0@users.noreply.github.com>
2022-06-25 16:30:05 +0900

    Update coin.py

--- Simple/coin.py
+++ Simple/coin.py
@@ -1,1 +1,25 @@
 
+
+coin500 = int(input("500엔 입력: "))
+coin100 = int(input("100엔 입력: "))
+coin50 = int(input("50엔 입력: "))
+coin10 = int(input("10엔 입력: "))
+coin5 = int(input("5엔 입력: "))
+coin1 = int(input("1엔 입력: "))
+
+a = 500*coin500
+b = 100*coin100
+c = 50*coin50
+d = 10*coin10
+e = 5*coin10
+f = 1*coin1
+print("")
+print("500엔은 총 ", a, "엔")
+print("100엔은 총 ", b, "엔")
+print("50엔은 총 ", c, "엔")
+print("10엔은 총 ", d, "엔")
+print("5엔은 총 ", e, "엔")
+print("1엔은 총 ", f, "엔")
+
+
+print(a+b+c+d+e+f, "엔")
